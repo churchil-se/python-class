2e0c93be8bea92236262fc006a1e7a46684c9eb5
churchil-se <okwachchurchil@gmail.com>
2025-03-22 11:38:08 +0300

    Added two files (list.py and week2assignment.py).

---
+++ list.py
@@ -0,0 +1,13 @@
+#Create a list 
+my_list = ["Sponges", "Bread", "Sugar", "Coffee"]
+
+print("List1:", my_list)
+
+his_list = ["Pens", "Books", "Bag", "Phone"]
+
+print("List2:", his_list)
+
+#Join two lists
+my_list.extend(his_list)
+
+print("Shopping List:", my_list)
